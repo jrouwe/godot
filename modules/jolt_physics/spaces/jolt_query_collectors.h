8c0da2189bf8ada0632004070e585c28690131d2
Jorrit Rouwe <jrouwe@gmail.com>
2026-01-17 16:27:34 +0100

    Fixed difference in return value between PhysicsServer3D.body_test_motion with and without result

--- modules/jolt_physics/spaces/jolt_query_collectors.h
+++ modules/jolt_physics/spaces/jolt_query_collectors.h
@@ -189,7 +189,7 @@ public:
 };
 
 template <typename TBase, int TDefaultCapacity>
-class JoltQueryCollectorClosestMulti final : public TBase {
+class JoltQueryCollectorClosestMulti : public TBase {
 public:
 	typedef typename TBase::ResultType Hit;
 	typedef JPH::Array<Hit, JPH::STLLocalAllocator<Hit, TDefaultCapacity + 1>> HitArray;
